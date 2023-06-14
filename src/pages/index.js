014f2549e4e9535f6809a2ab881cb65bb8e58111
Inzimam <hi@inhaq.me>
2023-06-14 21:17:34 +0500

    home page and real data added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,21 @@
 
 import React from 'react';
+import {Box, Flex, Link } from "@chakra-ui/react";
+import {BsGithub} from "react-icons/bs";
 
 const HomePage = () => {
-    return `Hello World`;
+    return (
+        <Flex alignItems={"center"} textAlign={"center"} flexDir={"column"} justifyContent={"center"} minH={"100vh"} background={"whatsapp.900"}>
+            <Flex flexDir={"column"} mt={"-150px"}>
+                <Box as={"h1"} fontSize={"60px"} fontWeight={"600"} color={"#44e660"}>LinkTree</Box>
+                <Box as={"h3"} fontSize={"40px"} color={"khaki"}>Everything in one link</Box>
+            </Flex>
+            <Box as={"p"} fontSize={"26px"} mt={20} color={"khaki"}>Example profiles:</Box>
+            <Link as={"a"} fontSize={"18px"} color={"teal.200"} href={"./inzi"}>Inzi 👨🏻‍💻</Link>
+            <Link as={"a"} fontSize={"18px"} color={"teal.200"} href={"./dan"}>Dan 🔥</Link>
+            <Box as={"p"} fontSize={"16px"} mt={8} color={"khaki"} maxW={"300px"}>Create your own account just by adding to data.json file in <Link as={"a"} color={"teal.200"} href={"https://github.com/inhaq/link-in-bio"}> Repo</Link></Box>
+        </Flex>
+
+    );
 }
 export default HomePage;
